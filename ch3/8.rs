60cd0682ebbc4453b0f349f99bfec77cb7152286
PurnaChandraReddy13 <22pa1a0526@vishnu.edu.in>
2025-07-21 10:24:10 +0530

    Create 8.rs

---
+++ ch3/8.rs
@@ -0,0 +1,15 @@
+/*8. Write a program to Print the following patterns
+&
+&&
+&&&
+&&&&
+&&&&&*/
+
+fn main() {
+    for i in 1..=5 {
+        for _ in 0..i {
+            print!("&");
+        }
+        println!();
+    }
+}
